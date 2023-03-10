64a7a00310e1ce75e7c53e3a38aa68509c34c86e
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-03-10 19:08:03 +0900

    header create

--- src/App.js
+++ src/App.js
@@ -1,42 +1,12 @@
 import { useState } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import "./App.css";
+import { Header } from "./components/header/Header";
 
 export const App = () => {
-  const [items] = useState([
-    { id: 0, text: "item0" },
-    { id: 1, text: "item1" },
-    { id: 2, text: "item2" },
-  ]);
-  const onDragEnd = (result) => {
-    const remove = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, remove[0]);
-  };
   return (
-    <div className="dragDropArea">
-      <DragDropContext onDragEnd={onDragEnd}>
-        <Droppable droppableId="droppable">
-          {(provided) => (
-            <div {...provided.droppableProps} ref={provided.innerRef}>
-              {items.map((item, index) => (
-                <Draggable draggableId={item.text} index={index} key={item.id}>
-                  {(provided) => (
-                    <div
-                      className="item"
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                    >
-                      {item.text}
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
+    <div>
+      <Header />
     </div>
   );
 };
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <div>
+      <header>
+        <h1>Simple Trello</h1>
+      </header>
+    </div>
+  );
+};
